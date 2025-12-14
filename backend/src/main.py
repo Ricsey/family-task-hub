09c380f11100d73e0ed5ddaeffc6c324664201aa
Ricsi Allili <rallili@2a01-036d-0121-2804-0b6c-a5bf-8c64-b390.pool6.digikabel.hu>
2025-12-14 22:07:46 +0100

    Create Hero class
    Creating Hero class from tutorial

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,80 @@
-from fastapi import FastAPI
+from typing import Annotated
 
-app = FastAPI()
+from fastapi import Depends, FastAPI, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+# async def lifespan(app: FastAPI):
+#     create_db_and_tables()
+#     yield
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# app = FastAPI(lifespan=lifespan)
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "ok"}
+
+
+@app.post("/heroes/", status_code=201)
+async def create_hero(hero: Hero, session: SessionDep):
+    session.add(hero)
+    session.commit()
+    session.refresh(hero)
+    return hero
+
+
+@app.get("/heroes/{hero_id}")
+async def read_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    return hero
+
+
+@app.get("/heroes/")
+async def read_heroes(session: SessionDep):
+    heroes = session.exec(select(Hero)).all()
+    return heroes
+
+
+@app.delete("/heroes/{hero_id}")
+async def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"ok": True}
