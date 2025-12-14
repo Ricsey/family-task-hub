3d5be1f693fcf21dcc6cf0f024e69bcd4c0debba
Ricsi Allili <rallili@2a01-036d-0121-2804-0b6c-a5bf-8c64-b390.pool6.digikabel.hu>
2025-12-14 23:34:33 +0100

    delete(hero): Delete Hero implementation
    We don't need this anymore

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-from src.api.routes.heroes import router as heroes_router
 from src.api.routes.users import router as users_router
 from src.core.db import create_db_and_tables
 
@@ -18,5 +17,4 @@ async def healthcheck():
     return {"status": "ok"}
 
 
-app.include_router(heroes_router)
 app.include_router(users_router)
