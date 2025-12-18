78e2e0ba90e5883a3c1981ac1ef4012158a94309
Ricsi Allili <rallili@2a01-036d-0121-411e-2c72-d7f1-22f2-857d.pool6.digikabel.hu>
2025-12-18 16:04:46 +0100

    Setup and config alembic

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote_plus
+
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -14,13 +16,16 @@ class Settings(BaseSettings):
     POSTGRES_DB: str = Field(init=False)
     POSTGRES_USER: str = Field(init=False)
     POSTGRES_PASSWORD: str = Field(init=False)
-    POSTGRES_SERVER: str = Field("db", init=False)  # Default value for Docker setup
+    POSTGRES_SERVER: str = Field(
+        "localhost", init=False
+    )  # Default value for Docker setup
     POSTGRES_PORT: str = Field("5432", init=False)
 
     @property
     def database_url(self) -> str:
+        password_encoded = quote_plus(self.POSTGRES_PASSWORD)
         return (
-            f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+            f"postgresql+psycopg2://{self.POSTGRES_USER}:{password_encoded}@"
             f"{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
 
