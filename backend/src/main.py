259ac42203b04b4a1bd52aa50e25bd01a490e58d
Ricsi Allili <rallili@2a01-036d-0121-411e-2c72-d7f1-22f2-857d.pool6.digikabel.hu>
2025-12-19 11:16:34 +0100

    chore: remove unnecessary commit

--- backend/src/main.py
+++ backend/src/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-    ],  # The URL of your React dev server
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
