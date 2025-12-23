7ee08caaaec08b437567a1c20f2fac3045ceb781
Ricsi Allili <rallili@2a01-036d-0121-411e-2c72-d7f1-22f2-857d.pool6.digikabel.hu>
2025-12-23 21:29:14 +0100

    Setup react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import TasksPage from './pages/TasksPage';
 
 function App() {
-  return <TasksPage />;
+  // return <TasksPage />;
+  return (
+    <div>
+      <div>Navbar</div>
+      <main>
+        <Routes>
+          <Route path="/tasks" element={<TasksPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
