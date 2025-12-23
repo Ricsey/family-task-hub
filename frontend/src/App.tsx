62e555f039b9b357a6b1a7f2e9d1d74a87db4b23
Ricsi Allili <rallili@2a01-036d-0121-2729-96ea-0d5e-b86c-a8f1.pool6.digikabel.hu>
2025-12-24 00:48:38 +0100

    WIP: Add task modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar';
+import TaskEditModal from './components/task/TaskEditModal';
+import { useTaskModal } from './hooks/tasks';
 import TasksPage from './pages/TasksPage';
 
 function App() {
-  // return <TasksPage />;
+  const { isOpen, editingTask, openAddModal, openEditModal, closeModal } =
+    useTaskModal();
   return (
     <div>
-      <Navbar />
+      <Navbar onAddTask={openAddModal} />
       <main className="min-h-screen bg-stone-50">
         <Routes>
           <Route path="/tasks" element={<TasksPage />} />
         </Routes>
       </main>
+      <TaskEditModal />
     </div>
   );
 }
