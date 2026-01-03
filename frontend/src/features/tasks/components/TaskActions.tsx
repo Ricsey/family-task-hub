2589089b8e05dbdf6f57dceae38f99ee3e570a04
richard_allili_innosw_hu <richard.allili@innosw.hu>
2026-01-03 22:57:11 +0100

    Add Confirmation dialog before delete task

--- frontend/src/features/tasks/components/TaskActions.tsx
+++ frontend/src/features/tasks/components/TaskActions.tsx
@@ -1,3 +1,13 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -5,7 +15,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { AlertDialogCancel } from '@radix-ui/react-alert-dialog';
 import { MoreHorizontal, Pencil, Trash2 } from 'lucide-react';
+import { useState } from 'react';
 import { useDeleteTask } from '../hooks/useTasks';
 import { useTaskModal } from '../stores/taskModalStore';
 import type { Task } from '../types';
@@ -17,40 +29,63 @@ interface TaskActionsProps {
 
 const TaskActions = ({
   task,
-  // onDelete
-}: TaskActionsProps) => {
-  const {openEditModal} = useTaskModal()
-  const {mutate: deleteTask} = useDeleteTask()
+}: // onDelete
+TaskActionsProps) => {
+  const { openEditModal } = useTaskModal();
+  const { mutate: deleteTask } = useDeleteTask();
+  const [showDeleteConfirmDialog, setShowDeleteConfirmDialog] = useState(false);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="h-8 w-8 shrink-0">
-          <MoreHorizontal className="w-4 h-4 text-stone-500" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-        onClick={() => openEditModal(task)}
-        >
-          <Pencil className="w-4 h-4 mr-2" />
-          Edit
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          // onClick={() => onDelete(task.id)}
-          onClick={() => {
-            // "Are you sure?" confirmation
-            if (confirm("Are you sure you want to delete this task?")) {
-              deleteTask(task.id)
-            }
-          }}
-          className="text-red-600 focus:text-red-600"
-        >
-          <Trash2 className="w-4 h-4 mr-2 text-red-600" />
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon" className="h-8 w-8 shrink-0">
+            <MoreHorizontal className="w-4 h-4 text-stone-500" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => openEditModal(task)}>
+            <Pencil className="w-4 h-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => setShowDeleteConfirmDialog(true)}
+            className="text-red-600 focus:text-red-600"
+          >
+            <Trash2 className="w-4 h-4 mr-2 text-red-600" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <AlertDialog
+        open={showDeleteConfirmDialog}
+        onOpenChange={setShowDeleteConfirmDialog}
+      >
+        <AlertDialogTrigger asChild></AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confirm deletion</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogDescription>
+            Are you sure you want to delete {task.title} task? This action
+            cannot be undone!
+          </AlertDialogDescription>
+          <AlertDialogFooter className='gap-2'>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-red-600 hover:bg-red-700 text-white"
+              onClick={() => {
+                deleteTask(task.id);
+                setShowDeleteConfirmDialog(false);
+              }}
+            >
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
